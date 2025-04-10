9b6d731c633cab4ba22011394c98b5afcdecb459
arodriguez-20199620 <arodriguez-2019620@kinal.edu.gt>
2025-04-10 14:24:26 -0600

    Cors actualizados

--- config/app.js
+++ config/app.js
@@ -22,12 +22,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "*"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 
